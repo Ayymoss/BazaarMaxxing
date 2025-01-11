969767ee8c9772709ffc425f450e9eab25af02ba
t0r0nt00 <njjh01@gmail.com>
2025-01-11 15:54:38 -0500

    added npc sell profit

--- BazaarCompanion/Models/OrderMeta.cs
+++ BazaarCompanion/Models/OrderMeta.cs
@@ -2,8 +2,9 @@
 
 public class OrderMeta
 {
-    public required double PotentialProfitMultiplier { get; set; }
-    public required double Margin { get; set; }
-    public required double MarginPercentage { get; set; }
-    public required double TotalWeekVolume { get; set; }
+    public double PotentialProfitMultiplier { get; set; }
+    public double Margin { get; set; }
+    public double MarginPercentage { get; set; }
+    public double TotalWeekVolume { get; set; }
+    public double NpcProfit { get; set; } // New property
 }
