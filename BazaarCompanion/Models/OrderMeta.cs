50e3014a81681550839193ac5908802802fd50b4
t0r0nt00 <njjh01@gmail.com>
2025-01-11 17:12:55 -0500

    Added NPC Profit and Profit Margin, Merged in other login

--- BazaarCompanion/Models/OrderMeta.cs
+++ BazaarCompanion/Models/OrderMeta.cs
@@ -6,4 +6,6 @@ public class OrderMeta
     public required double Margin { get; set; }
     public required double MarginPercentage { get; set; }
     public required double TotalWeekVolume { get; set; }
+    public decimal NpcProfit { get; set; }
+    public decimal NpcMargin { get; set; }
 }
--- BazaarCompanion/Models/ProductData.cs
+++ BazaarCompanion/Models/ProductData.cs
@@ -8,4 +8,6 @@ public class ProductData
     public required OrderInfo Buy { get; set; }
     public required OrderInfo Sell { get; set; }
     public required OrderMeta OrderMeta { get; set; }
+    public decimal NpcProfit { get; set; }
+    public decimal NpcMargin { get; set; }
 }
