a7aca163c12795f794245c1981d10f08e2fa995b
Amos <github@amosr.uk>
2026-01-31 12:08:44 +0000

    db: apply AddSkinUrlToProduct migration

---
+++ BazaarCompanionWeb/Migrations/20260131120400_AddSkinUrlToProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BazaarCompanionWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSkinUrlToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SkinUrl",
+                table: "EFProducts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SkinUrl",
+                table: "EFProducts");
+        }
+    }
+}
